e1d85c9ab8222cd6eda3c9aa79a30cfe61d38403
Yasith Rashan <154666486+yasithrashan2003@users.noreply.github.com>
2025-04-17 19:30:43 +0530

    Create a Java Application and Book Class

---
+++ CSACourseWork/src/main/java/com/mycompany/csacoursework/CSACourseWork.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csacoursework;
+
+/**
+ *
+ * @author Yasith
+ */
+public class CSACourseWork {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
